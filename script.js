fca110d3972e4908ad83a7f9990b4f6b67f2d58f
Lex95 <andrea.lega1995@gmail.com>
2021-04-21 16:22:18 +0200

    risoluzione esercizio

--- script.js
+++ script.js
@@ -1,33 +1,29 @@
-var numbers = [];
 var MAX_NUMBERS = 5;
 var TIMER = 30;
+var numbers = [];
+var userNumbers = [];
+var validNumbers = [];
+var counter = 0;
 
 for (var i = 0; i < MAX_NUMBERS; i++) {
     numbers.push(Math.ceil(Math.random() * 100));
 }
 alert(numbers.join(" - "))
 
-var userNumbers = [];
-
 function askNumbers(arrayNumbers) {
     for (i = 0; i < numbers.length; i++) {
         arrayNumbers.push(parseInt(prompt("Inserisci uno alla volta i numeri che ti ricordi:")));
     }
-    return arrayNumbers;
 }
 
-setTimeout(askNumbers(userNumbers), (1000 * TIMER));
-// clearTimeout(askNumbers);
-
-var counter = 0;
-var validNumbers = [];
-
-for (i = 0; i < numbers.length; i++) {
-    if (numbers.indexOf(userNumbers[i]) >= 0) {
-        counter++
-        validNumbers.push(userNumbers[i]);
+setTimeout(function () {
+    askNumbers(userNumbers);
+    for (i = 0; i < numbers.length; i++) {
+        if (numbers.indexOf(userNumbers[i]) >= 0) {
+            counter++
+            validNumbers.push(userNumbers[i]);
+        }
     }
-}
 
-document.getElementById("myH1").innerHTML = "Hai indovinato " + counter + " numeri!<br>" + validNumbers.join(" ");
-
+    document.getElementById("myH1").innerHTML = "Hai indovinato " + counter + " numeri!<br>" + validNumbers.join(" ");
+}, (1000 * TIMER));
