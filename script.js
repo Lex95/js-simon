05c522bd357c390b428dd3370f353d49b1614a76
Lex95 <andrea.lega1995@gmail.com>
2021-04-21 16:04:15 +0200

    aggiunta prima risoluzione

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 var numbers = [];
 var MAX_NUMBERS = 5;
-var TIMER = 5;
+var TIMER = 30;
 
-function numberGenerator(num) {
-    for (var i = 0; i < num; i++) {
-        numbers.push(Math.ceil(Math.random() * 100));
+for (var i = 0; i < MAX_NUMBERS; i++) {
+    numbers.push(Math.ceil(Math.random() * 100));
+}
+alert(numbers.join(" - "))
+
+var userNumbers = [];
+
+function askNumbers(arrayNumbers) {
+    for (i = 0; i < numbers.length; i++) {
+        arrayNumbers.push(parseInt(prompt("Inserisci uno alla volta i numeri che ti ricordi:")));
     }
-    alert(numbers.join(" "))
+    return arrayNumbers;
 }
 
-var numberDisplayer = setInterval(numberGenerator(MAX_NUMBERS), (1000 * TIMER));
+setTimeout(askNumbers(userNumbers), (1000 * TIMER));
+// clearTimeout(askNumbers);
 
-clearInterval(numberDisplayer);
+var counter = 0;
+var validNumbers = [];
+
+for (i = 0; i < numbers.length; i++) {
+    if (numbers.indexOf(userNumbers[i]) >= 0) {
+        counter++
+        validNumbers.push(userNumbers[i]);
+    }
+}
+
+document.getElementById("myH1").innerHTML = "Hai indovinato " + counter + " numeri!<br>" + validNumbers.join(" ");
+
